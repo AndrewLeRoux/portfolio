f88daa034c4726e7a96b6a4842208b5e55a13f67
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-08-02 15:37:53 -0400

    updated project list

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,12 +5,12 @@ import TranaImg from "../images/Trana.jpg"
 
 
 export const ProjectList = [
-    {
-        name: "Activity planner",
-        image: ActivityPlannerImg,
-        description: "a website where users can find new activities to do or favorite activities that they want to participate in",
-        link: ""
-    },
+    // {
+    //     name: "Activity planner",
+    //     image: ActivityPlannerImg,
+    //     description: "a website where users can find new activities to do or favorite activities that they want to participate in",
+    //     link: ""
+    // },
     {
         name: "Andy's List",
         image: AndysListImg,
